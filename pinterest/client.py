45bbdd9f30819a084f4aefe376e3d99030b3c115
Rich Schumacher <rich.schu@gmail.com>
2012-08-31 17:59:01 -0400

    Major refactor of the API client.
     * Added many new API endpoints.
     * Renamed existing endpoints to try to keep a consistent naming
       convention.
     * Removed committed .pyc files.
     * Added requests to setup.py and removed simplejson.

--- pinterest/client.py
+++ pinterest/client.py
@@ -1,16 +1,27 @@
-import urllib, urllib2
+import time
+import json
+import requests
+
 
 def ensure_access_token(f):
     def g(self, *args, **kwargs):
         if not self.access_token:
             raise PinterestException("You need an access token to make that call")
-        f(self, *args, **kwargs)
+        return f(self, *args, **kwargs)
     return g
 
+
 class PinterestAPI(object):
-    prefix_path = "https://api.pinterest.com/v2/"
-    authorize_url = "https://api.pinterest.com/oauth/authorize"
-    access_token_url = "https://api.pinterest.com/oauth/access_token"
+    base_url = "https://api.pinterest.com"
+    authorize_url = "%s/oauth/authorize" % base_url
+    access_token_url = "%s/oauth/access_token" % base_url
+    headers = {
+        "Accept-Language": "en",
+        "Accept-Encoding": "gzip",
+        "Host": base_url.replace("https://", ""),
+        "User-Agent":  "Pinterest for Android/1.0.2 (generic_x86; 4.0.4)",
+        "X-Pixel-Ratio": "1.5",
+    }
 
     def __init__(self, access_token=None):
         self.access_token = access_token
@@ -18,49 +29,186 @@ class PinterestAPI(object):
     @ensure_access_token
     def _get_request(self, path, params={}):
         params["access_token"] = self.access_token
-        url = "%s%s?%s" %(self.prefix_path, path, urllib.urlencode(params))
+        url = "%s%s" % (self.base_url, path)
         try:
-            return urllib2.urlopen(url).read()
-        except urllib2.HTTPError, err:
-            raise PinterestException(err.read())
-    
+            response = requests.get(url, params=params, headers=self.headers)
+        except requests.exceptions.RequestException, rex:
+            raise PinterestException(rex)
+
+        return json.loads(response.text)
+
+    # Figure how to selectively enforce access_token here
     def _post_request(self, path, params={}, data={}):
-        params["access_token"] = self.access_token
-        url = "%s%s?%s" %(self.prefix_path, path, urllib.urlencode(params))
+        url = "%s%s" % (self.base_url, path)
         try:
-            return urllib2.urlopen(url,urllib.urlencode(data)).read()
-        except urllib2.HTTPError, err:
-            raise PinterestException(err.read())
+            return requests.post(url, params=params, data=data,
+                headers=self.headers)
+        except requests.exceptions.RequestException, rex:
+            raise PinterestException(rex)
 
-    def get_homefeed(self, page=1, limit=20):
-        return self._get_request("home", {"page": page, "limit": limit})
+    def login(self, username, password):
+        """Obtain an OAuth access token to be used for this session."""
+        path = "/v1/login/?client_id=1431598&timestamp=%d&oauth_signature=%s"
 
-    def get_pin(self, pin_id):
-        return self._get_request("pin/%d" %(pin_id))
-        
-    def get_popular(self, page=1, limit=20):
-        return self._get_request("popular", {"page": page, "limit": limit})
-        
-    def get_all(self, category="", page=1, limit=20):
-        return self._get_request("all", {"category": category,"page": page, "limit": limit})
+        # XXX: Does not work currently; need to figure out how to generate
+        # the oauth_signature parameter.
+        raise NotImplementedError()
 
-    def get_videos(self, page=1, limit=20):
-        return self._get_request("videos", {"page": page, "limit": limit})
-        
-    def get_boards(self):
-        return self._get_request("boards")
-        
-    def get_activity(self):
-        return self._get_request("activity")
-    
-    #domain must be "pins","boards" or "people"
-    def get_search(self, domain="pins", query=""):
-        return self._get_request("search/%d" %(domain), {"query": query})
-        
+        timestamp = int(time.time())
+        oauth_sig = ""
+        path = path % (timestamp, oauth_sig)
+
+        data = {"username": username, "password": password}
+        response = self._post_request(path, data=data)
+        content = json.loads(response.text)
+
+        if response.status_code != 200:
+            raise PinterestException(content["error"])
+
+        self.access_token = content["access_token"]
+        return self.access_token
+
+    def me(self):
+        """Retrieve the currently authenticated user."""
+        path = "/v2/me"
+        return self._get_request(path)
+
+    def home(self, page=1, limit=25):
+        """Retreive the home feed for the currently authenticated user."""
+        path = "/v2/home"
+        return self._get_request(path, {"page": page, "limit": limit})
+
+    def boards(self):
+        """Retrieve all the boards for the currently authenticated user."""
+        path = "/v2/boards"
+        return self._get_request(path)
+
+    def board(self, user, board):
+        """Retrieve information about the specified user's board."""
+        path = "/v2/boards/%s/%s" % (user, board)
+        return self._get_request(path)
+
+    def pin(self, pin_id):
+        """Retrieve information about a single Pin."""
+        path = "/v2/pin/%d" % pin_id
+        return self._get_request(path)
+
+    def popular(self, page=1, limit=25):
+        """Retrieve a list of popular Pins."""
+        path = "/v2/popular"
+        return self._get_request(path, {"page": page, "limit": limit})
+
+    def all(self, category=None, page=1, limit=25):
+        """Retrieve a list of Pins, possibly from a specific category. Valid
+        categories can be determined by the categories() method below.
+        """
+        path = "/v2/all"
+        return self._get_request(path, {"category": category, "page": page, "limit": limit})
+
+    def videos(self, page=1, limit=25):
+        """Retrieve a list of video Pins."""
+        path = "/v2/videos"
+        return self._get_request(path, {"page": page, "limit": limit})
+
+    def activity(self):
+        """Retrieve recent activity information for the currently authenticated user."""
+        path = "/v2/activity"
+        return self._get_request(path)
+
+    def search(self, query, domain="pins", page=1, limit=25):
+        """Search for Pins in the given domain. Domain must be one of the
+        following: pins, boards, people.
+        """
+        path = "/v2/search/%s" % domain
+        return self._get_request(path, {"query": query, "page": page, "limit": limit})
+
+    def categories(self):
+        """Retrieve the list of available categories."""
+        path = "/v3/categories"
+        return self._get_request(path)
+
+    def user(self, user):
+        """Retreive information about the given user."""
+        path = "/v2/users/%s" % user
+        return self._get_request(path)
+
+    def _user_info(self, user, info_type, page=1, limit=25):
+        """Helper method to retrieve specific data for the given user."""
+        path = "/v2/users/%s/%s/" % (user, info_type)
+        return self._get_request(path, {"page": page, "limit": limit})
+
+    def user_boards(self, user, page=1, limit=25):
+        """Retrieve all information about the given user's boards."""
+        return self._user_info(user, "boards", page, limit)
+
+    def user_pins(self, user, page=1, limit=25):
+        """Retrieve the given user's Pins."""
+        return self._user_info(user, "pins", page, limit)
+
+    def user_likes(self, user, page=1, limit=25):
+        """Retrieve the given user's Likes."""
+        return self._user_info(user, "likes", page, limit)
+
+    def user_about(self, user):
+        """Retrieve the about section for the given user."""
+        return self._user_info(user, "about")
+
+    def following(self, user):
+        """Retrieve the list of users the given user is following. Answers the
+        question: Who is <USER> following?
+        """
+        path = "/v2.1/users/%s/following" % user
+        return self._get_request(path)
+
+    def followers(self, user):
+        """Retrieve the list of users who are following the given user. Answers
+        the question: Who is following <USER>?
+        """
+        path = "/v2.1/users/%s/followers" % user
+        return self._get_request(path)
+
+    def follow(self, user):
+        """Request that the currently authenticated user follow another user."""
+        raise NotImplementedError()
+        path = "/v2/user/%s/follow/" % user
+        return self._post_request(path)
+
+    def unfollow(self, user):
+        """Request that the currently authenticated user unfollow another user."""
+        raise NotImplementedError()
+        path = "/v2/user/%s/unfollow/" % user
+        return self._post_request(path)
+
+    def repin(self, pin_id, board_id, details=None, publish_facebook=False, publish_twitter=False):
+        """Repin the specified Pin to the specified Board."""
+        raise NotImplementedError()
+        path = "/v2/repin/%d/" % pin_id
+        data = {
+            "board": board_id,
+            "detail": details,
+            "publish_to_facebook": publish_facebook,
+            "publish_to_twitter": publish_twitter
+        }
+        return self._post_request(path, data=data)
+
+    def like(self, pin_id):
+        """Like the specified Pin."""
+        raise NotImplementedError()
+        path = "/v2/pin/%d/like/" % pin_id
+        return self._post_request(path)
+
+    def comment(self, pin_id, text):
+        """Leave a comment on the specified Pin."""
+        raise NotImplementedError()
+        path = "/v2/pin/%d/comment/" % pin_id
+        return self._post_request(path, data={"text": text})
+
+    def create_board(self, name, category=None, description=None):
+        """Create a new board."""
+        raise NotImplementedError()
+        path = "/v2/board/create/"
+        return self._post_request(path, data={"name": name, "category": category, "description": description})
+
+
 class PinterestException(Exception):
-    def __init__(self, description):
-        self.description = description
-
-    def __str__(self):
-        return self.description
-
+    pass
